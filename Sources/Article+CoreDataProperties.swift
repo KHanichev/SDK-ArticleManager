6e37f35d03b5e2adf71060039e29e4dbe5b8fd1d
KHanichev <79635762+KHanichev@users.noreply.github.com>
2021-02-26 13:35:23 +0200

    Update Article+CoreDataProperties.swift
    version 0.1.1

--- Sources/Article+CoreDataProperties.swift
+++ Sources/Article+CoreDataProperties.swift
@@ -11,10 +11,6 @@ import CoreData
 
 extension Article {
 
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<Article> {
-        return NSFetchRequest<Article>(entityName: "Article")
-    }
-
     @NSManaged public var title: String?
     @NSManaged public var content: String?
     @NSManaged public var language: String?
